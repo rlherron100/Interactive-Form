334ec0f1b1216c90d9692d6a19547981341f500b
robherron <rlherron100@gmail.com>
2020-05-26 16:36:22 -0400

    Updated t-shirt functionality

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ setFocusToNameBox();
 
 
 
-
  const title=document.getElementById('title');
 
 x = document.createElement('INPUT');
@@ -34,48 +33,58 @@ x.style.display='none';
         }
  };
 
-const selectDesign = document.createElement('OPTION');
-const color = document.getElementById('color');
-const colorOptions=color.children;
-const design = document.getElementById('design');
-selectDesign.classList.add('selectDesign');
+const selectDesign = document.createElement('OPTION');      //creates an option element
+const color = document.getElementById('color');             //creates variable named 'color' with the value of the color selection
+const design = document.getElementById('design');           //creates variable named 'design' with the value of the design selection
+
+
+selectDesign.classList.add('selectDesign');     
 selectDesign.innerHTML="Please select a design option";
 color.insertBefore(selectDesign, color.firstChild);
+design.options[0].selected=true;
 
 
-// for (var i=0; i < color.length; i++){
-//     color[i].style.display='none';
-//     if (i != 7){color.options[i].selected=false
-//     } else {
-//         color.options[i].selected=true
-//     }
-// }
-// for (var i=0; i<color.length; i ++) {
+for (let i=1; i < color.options.length; i++) {
+if (design.options[0].selected=true) { 
+    color.options[0].selected=true; 
+    color.options[i].style.display='none';
+}
+
+}
+
+onchange = function changeDesignOptions () {
     
-//     if (color.options[i].innerText.includes('please')) 
-//     {color[i].style.display='block'; 
-//     color.options[7].selected=true;
-//     }  else 
-//     {color[i].style.display = 'none'
-// }
-// }
+        if (design.options[1].selected) {
+            color.options[1].selected=true;
+            color.options[0].style.display='none';
+            color.options[4].style.display='none';
+            color.options[5].style.display='none';
+            color.options[6].style.display='none';
+            color.options[1].style.display='block';
+            color.options[2].style.display='block';
+            color.options[3].style.display='block';
+        }
+        else if (design.options[2].selected) {
+            color.options[4].selected=true;
+            color.options[0].style.display='none';
+            color.options[1].style.display='none';
+            color.options[2].style.display='none';
+            color.options[3].style.display='none';
+            color.options[4].style.display='block';
+            color.options[5].style.display='block';
+            color.options[6].style.display='block';
+        }
+        else {
+            design.options[0].selected=true;
+            color.options[0].selected=true;
+            color.options[0].style.display='block';
+            color.options[1].style.display='none';
+            color.options[2].style.display='none';
+            color.options[3].style.display='none';
+            color.options[4].style.display='none';
+            color.options[5].style.display='none';
+            color.options[6].style.display='none';
 
-for (var i=0; i < color.length; i++) {
-    if (design[i] === 0) {
-        color.options[0].selected=true;
-        color.options[1,2,3,4,5,6].selected=false
-} else if (design[i] <4 && design.options[i] > 0) {
-    color.options[i].style.display='block';
-    color.options[0,5,6].style.display='none';
-}
-else {
-    color.options[i].style.display='block';
-    color.options[0,1,2,3,4].style.display='none';
-}
-}
+        }
+    }
 
-
-
-
-// If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
-// If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
