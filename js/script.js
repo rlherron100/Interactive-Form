c74fd15223ff1d2dc10d69427f3b7a7a7893975f
robherron <rlherron100@gmail.com>
2020-06-09 22:55:21 -0400

    Added validators and cleaned up code

--- js/script.js
+++ js/script.js
@@ -34,36 +34,92 @@ bitCoin.style.display='none';
 
 
 
+document.querySelectorAll('.activities').setAttribute('id', 'activities'); //NEED TO GIVE AN ID ATTRIBUTE TO THE ACTIVITIES FIELDSET SO THAT I CAN INSERT A NODE UNDER IT
+
+
+
+
+
+
 //REGEX SECTION
 
 
 let validName = false;
 let validEmail = false;
+let validActivities = false;
 let validCardNum = false;
 let validZip = false;
 let validCvv = false;
 let name = document.getElementById('name');
 let mail = document.getElementById('mail');
+let ccNum= document.getElementById('cc-num');
+let zip = document.getElementById('zip');
+let cvv= document.getElementById('cvv');
 let fieldset = document.getElementById('basics');
+let ccDiv= document.getElementById('credit-card');
 const invalidName =  document.createElement('LABEL');
-invalidName.classList.add('invalidName')
-
-document.querySelector('button[type="submit"]').setAttribute('id', 'submit')
+const invalidEmail =  document.createElement('LABEL');
+const invalidActivities= document.createElement('LABEL');
+const invalidccNum = document.createElement('LABEL');
+const invalidZip = document.createElement('LABEL');
+const invalidCVV = document.createElement('LABEL');
 const submit = document.getElementById('submit');
-
-fieldset.insertBefore(invalidName, fieldset.children[3]);
+//Name Validator Element
+invalidName.classList.add('invalidName')
 invalidName.innerHTML ="Please enter a valid name.";
 invalidName.style.display='none';
+//Email Validator Element
+invalidEmail.classList.add('invalidEmail');
+invalidEmail.innerHTML ="Please enter a valid email.";
+fieldset.insertBefore(invalidEmail, fieldset.children[5]);
+invalidEmail.style.display= 'none';
+//Activities Validator Element
+invalidActivities.classList.add('invalidActivities');
+invalidActivities.innerHTML= "You must select at least one activity.";
+activities.insertBefore(invalidActivities, activityChecks[0]);
+invalidActivities.style.display='none';
+//Credit Card Number Validator Element
+invalidccNum.classList.add('invalidccNum');
+invalidccNum.innnerHTML = "Please enter a valid Credit Card Number.";
+invalidccNum.style.display='none';
+ccDiv.insertBefore(invalidccNum, ccDiv.children[3]);
+//Credit Card Zip Code Validator Element
+invalidZip.classList.add('invalidZip');
+invalidZip.innerHTML= "Please enter a valip zip code.";
+invalidZip.style.display='none';
+ccDiv.insertBefore(invalidccNum, ccDiv.children[4]);
+//Credit Card CVV Validator Element
+invalidCVV.classList.add('invalidcvv');
+invalidCVV.innerHTML= "Please enter a valid CVV.";
+invalidCVV.style.display='none';
+ccDiv.insertBefore(invalidccNum, ccDiv.children[5]);
 
-function nameValidator (inputName) {
+document.querySelector('button[type="submit"]').setAttribute('id', 'submit')
+fieldset.insertBefore(invalidName, fieldset.children[3]);
+
+
+function nameRegex (inputName) {
     return /^[a-z]+\s[a-z]+$/i.test(inputName);
 }
+function emailRegex (inputEmail) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(inputEmail); //source https://emailregex.com/
+}
+function ccNumRegex (inputCC) {/^[0-9]{13,16}$/.test(inputCC)};
+function zipRegex (inputZip) {/^[0-9]{5}(?:-[0-9]{4})?$/}; // from https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s14.html
+function cvvRegex (inputCVV) {/^[0-9]{3}$/};
+//blur event form validators
+name.addEventListener ('blur', () => {nameValidator()});
+mail.addEventListener ('blur', () => { emailValidator()});
+ccNum.addEventListener('blur', () => {ccNumValidator()})
+zip.addEventListener('blur', () => {zipValidator()} );
+cvv.addEventListener('blur', () => {cvvValidator()} );
 
-name.addEventListener ('blur', () => {
+
+function nameValidator() {
     
     let userName = name.value;
     
-    if (nameValidator(userName)) {
+    if (nameRegex(userName)) {
         name.style.border = '2px solid green';
         name.style.color = 'green';
         invalidName.style.display= 'none';
@@ -77,24 +133,14 @@ name.addEventListener ('blur', () => {
         return validName = false;
     }
     
-});
+};
 
-const invalidEmail =  document.createElement('LABEL');
-invalidEmail.classList.add('invalidEmail')
-invalidEmail.innerHTML ="Please enter a valid email."
-fieldset.insertBefore(invalidEmail, fieldset.children[6]);
-invalidEmail.style.display= 'none';
-
-function emailValidator (inputEmail) {
-        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(inputEmail); //source https://emailregex.com/
-    }
-
-    mail.addEventListener ('blur', () => {
-        let userEmail = mail.value;
+function emailValidator () {
+            let userEmail = mail.value;
         
-    ;
+    
         
-        if (emailValidator(userEmail)) {
+        if (emailRegex(userEmail)) {
             mail.style.border = '2px solid green';
             mail.style.color = 'green';
             invalidEmail.style.display= 'none';
@@ -109,45 +155,93 @@ function emailValidator (inputEmail) {
             return validEmail = false;
         }
         
-});
+};
+
+function activityValidator () {
+    for (let i = 0; i< activityChecks.length; i++)
+        if (activityChecks[i].checked)
+        {
+        activityChecks.style.border = '2px solid green';
+        activityChecks.style.color='green';
+        document.querySelectorAll('.invalidActivities').forEach(function(a) {
+            a.style.display='none';
+        })
+        return validActivities = true;
+    } else {
+        activityChecks.style.border = '3px solid red';
+        invalidActivities.style.display='block';
+        return validActivities=false;
+    }
+}
 
 
 
 
-  
-  
+function ccNumValidator () {let userccNum = ccNum.value;
+        
+    
+        
+        if (ccNumRegex(userccNum)) {
+            ccNum.style.border = '2px solid green';
+            ccNum.style.color = 'green';
+            invalidccNum.style.display= 'none';
+            document.querySelectorAll('.invalidccNum').forEach(function(a) {                        
+                a.style.display='none';
+            })
+            return validccNum = true;
+        } else {
+            ccNum.style.border = '3px solid red';
+            invalidccNum.style.display= 'block';
+            return validccNum = false;
+        }}
+function zipValidator () {
+        let userZip= zip.value;
+        
+            
+                
+                if (zipRegex(userZip)) {
+                    zip.style.border = '2px solid green';
+                    zip.style.color = 'green';
+                    invalidZip.style.display= 'none';
+                    document.querySelectorAll('.invalidZip').forEach(function(a) {                        
+                        a.style.display='none';
+                    })
+                    return validZip = true;
+                } else {
+                    zip.style.border = '3px solid red';
+                    invalidZip.style.display= 'block';
+                    return validZip = false;
+                }}
+function cvvValidator () {
+         let userCvv= cvv.value;
+        
+            
+                
+                if (cvvRegex(userCvv)) {
+                    cvv.style.border = '2px solid green';
+                    cvv.style.color = 'green';
+                    invalidCVV.style.display= 'none';
+                    document.querySelectorAll('.invalidcvv').forEach(function(a) {                        
+                        a.style.display='none';
+                    })
+                    return validCvv = true;
+                } else {
+                    cvv.style.border = '3px solid red';
+                    invalidCVV.style.display= 'block';
+                    return validCvv = false;
+                }};
+
  
-  form.addEventListener('submit', () => {
-    var isChecked = false;
-   
-    
-    for (var i = 0; i < activityChecks.length; i++) {
-            if (activityChecks[i].checked) {
-                    isChecked = true;
-                    break;
-            }
-    }
+  form.addEventListener('submit', (e) => {
+    nameValidator();
+    if (!nameValidator() && !emailValidator() && !activityValidator() && !ccNumValidator() && !zipValidator () && !cvvValidator())
+   {
+       e.preventDefault();
 
-    if (isChecked=false) {
-            console.log('please work');
-            alert("Please select image format from the list.");
-            activities.style.border = '3px solid red';
-    }
-
-    return isChecked;
+       window.scrollTo(0,0)}
+        
 });
 
-
-
-
-
-// submit.addEventListener('click', function validateActivities() {
-   
-// if (document.querySelector('button[type="submit"]')) {
-//     console.log('You must select at least one activity to submit.');
-// }
-    
-// })
 // -------------------
 
 
@@ -263,6 +357,3 @@ payment.onchange = function paymentOptionsToggle() {
             bitCoin.style.display='none';
         }
 }
-
-
-
